2e0aef7e4dee0ea8e1e7c74488eb2c15100e3eb9
chipmoreno <chip.moreno@gmail.com>
2025-01-21 18:18:15 -0500

    Client Side JS Practice

---
+++ Client Side JS/ElementSelection.js
@@ -0,0 +1,6 @@
+sect1 = document.getElementById("sect1");
+colors = document.getElementById("color");
+headings = document.getElementById("h1");
+subheads = document.getElementById("h2");
+tooltips = document.getElementById("tooltip");
+sidebars = document.getElementById("sidebar");
